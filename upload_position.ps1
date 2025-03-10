55d2056aa43266da97e57a3b3b3431dd95c02d90
huxiaoou <516984451@qq.com>
2025-03-10 17:44:40 +0800

    Init commit

---
+++ upload_position.ps1
@@ -0,0 +1,20 @@
+$currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$currentTime | Out-File -FilePath D:\PsScripts\test.log -Append
+
+Set-Location E:\Projects\DataManagerTushare\
+$bgn_date = Get-Date -Format "yyyyMMdd"
+$tyear = Get-Date -Format "yyyy"
+
+python main.py --bgn $bgn_date download --switch fmd
+python main.py --bgn $bgn_date download --switch contract
+python main.py --bgn $bgn_date download --switch universe
+python main.py --bgn $bgn_date download --switch position
+python main.py --bgn $bgn_date download --switch basis
+python main.py --bgn $bgn_date download --switch stock
+
+python main.py --bgn $bgn_date update --switch fmd
+python main.py --bgn $bgn_date update --switch position
+python main.py --bgn $bgn_date update --switch basis
+python main.py --bgn $bgn_date update --switch stock
+
+scp -r E:\OneDrive\Data\tushare\by_date\$tyear\$bgn_date sxzqhxo:/root/ProjectsData/input/by_date/$tyear
