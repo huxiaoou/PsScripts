ee7baf15c5b9581aa1df2d3125e65d5ccc9b5c79
huxiaoou <516984451@qq.com>
2025-03-14 11:17:59 +0800

    Update server location;Add a parameter to control script behavior when no date argument provided

--- upload_position.ps1
+++ upload_position.ps1
@@ -1,7 +1,10 @@
-Set-Location E:\Projects\DataManagerTushare\
-$bgn_date = Get-Date -Format "yyyyMMdd"
-$tyear = Get-Date -Format "yyyy"
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$bgn_date = (Get-Date -Format "yyyyMMdd")
+)
+$tyear = $bgn_date.Substring(0, 4)
 
+Set-Location E:\Projects\DataManagerTushare\
 python main.py --bgn $bgn_date download --switch fmd
 python main.py --bgn $bgn_date download --switch contract
 python main.py --bgn $bgn_date download --switch universe
@@ -14,4 +17,4 @@ python main.py --bgn $bgn_date update --switch position
 python main.py --bgn $bgn_date update --switch basis
 python main.py --bgn $bgn_date update --switch stock
 
-scp -r E:\OneDrive\Data\tushare\by_date\$tyear\$bgn_date sxzqhxo:/root/ProjectsData/input/by_date/$tyear
+scp -r E:\OneDrive\Data\tushare\by_date\$tyear\$bgn_date sxzqhxo:/root/workspace/Data/huxo/input/by_date/$tyear
