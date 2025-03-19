155f881b996f839223deb42e3eaf3c9fdcc71622
huxiaoou <516984451@qq.com>
2025-03-19 09:58:03 +0800

    Set task to dowanload and update position only

--- upload_position.ps1
+++ upload_position.ps1
@@ -5,16 +5,16 @@ param (
 $tyear = $bgn_date.Substring(0, 4)
 
 Set-Location E:\Projects\DataManagerTushare\
-python main.py --bgn $bgn_date download --switch fmd
-python main.py --bgn $bgn_date download --switch contract
-python main.py --bgn $bgn_date download --switch universe
+# python main.py --bgn $bgn_date download --switch fmd
+# python main.py --bgn $bgn_date download --switch contract
+# python main.py --bgn $bgn_date download --switch universe
 python main.py --bgn $bgn_date download --switch position
-python main.py --bgn $bgn_date download --switch basis
-python main.py --bgn $bgn_date download --switch stock
+# python main.py --bgn $bgn_date download --switch basis
+# python main.py --bgn $bgn_date download --switch stock
 
-python main.py --bgn $bgn_date update --switch fmd
+# python main.py --bgn $bgn_date update --switch fmd
 python main.py --bgn $bgn_date update --switch position
-python main.py --bgn $bgn_date update --switch basis
-python main.py --bgn $bgn_date update --switch stock
+# python main.py --bgn $bgn_date update --switch basis
+# python main.py --bgn $bgn_date update --switch stock
 
 scp -r E:\OneDrive\Data\tushare\by_date\$tyear\$bgn_date sxzqhxo:/root/workspace/Data/huxo/input/by_date/$tyear
