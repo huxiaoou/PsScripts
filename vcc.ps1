eecab6ebb6f09e59571bc511567eb0ddb89c3079
huxiaoou <516984451@qq.com>
2026-01-05 10:20:27 +0800

    Ad vcc.ps1

---
+++ vcc.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$td,
+    [string]$match
+)
+
+if ($td -eq "") { 
+    $td = Get-Date -Format yyyyMMdd 
+}
+$ty = $td.Substring(0, 4)
+$tm = $td.Substring(4, 2)
+
+view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
