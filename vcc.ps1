bf7d340a24ff99e0e7fe9e7fdc7f0299f03ab51b
huxiaoou <516984451@qq.com>
2026-01-08 16:24:44 +0800

    Udpate vcc.ps1

--- vcc.ps1
+++ vcc.ps1
@@ -9,4 +9,10 @@ if ($td -eq "") {
 $ty = $td.Substring(0, 4)
 $tm = $td.Substring(4, 2)
 
-view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
+Write-Host "---------------------------"
+
+view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td-huxo.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
+
+Write-Host "---------------------------"
+
+view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td-szst.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
