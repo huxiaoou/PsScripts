b107520f0973102089cd7f151fc402363a4e372a
huxiaoou <516984451@qq.com>
2026-01-09 15:42:54 +0800

    Update sync pos

--- sync_pos_to_deploy.ps1
+++ sync_pos_to_deploy.ps1
@@ -1,12 +1,26 @@
+function sync_file_to_remote_dir {
+    param (
+        [string]$src,
+        [string]$dst
+    )
+    Write-Host "[INF] Src " -NoNewline
+    Write-Host "${src}" -ForegroundColor Green
+    Write-Host "[INF] Dst " -NoNewline
+    Write-Host "${dst}" -ForegroundColor Green
+
+    scp $src $dst
+}
+
+
 $td = Read-Host -Prompt "Please input the trade_date for position file, format = [YYYYMMDD]"
 $ty = $td.Substring(0, 4)
 $tm = $td.Substring(4, 2)
-$src_path = "E:\Data\Projects\Deploy-Operation\positions-real\$ty\$tm\持仓汇总-$td.xls"
 $dst_path = "lianghua:E:\Data\huxo\Projects\Deploy-Operation\positions\$ty\$tm\"
 
-Write-Host "[INF] Src path " -NoNewline
-Write-Host "${src_path}" -ForegroundColor Green
-Write-Host "[INF] Dst path " -NoNewline
-Write-Host "${dst_path}" -ForegroundColor Green
+# --- huxo
+$src_path = "E:\Data\Projects\Deploy-Operation\positions-real\$ty\$tm\持仓汇总-$td-huxo.xls"
+sync_file_to_remote_dir -src $src_path -dst $dst_path
 
-scp ${src_path} ${dst_path}
+# --- szst
+$src_path = "E:\Data\Projects\Deploy-Operation\positions-real\$ty\$tm\持仓汇总-$td-szst.xls"
+sync_file_to_remote_dir -src $src_path -dst $dst_path
--- vcc.ps1
+++ vcc.ps1
@@ -10,9 +10,11 @@ $ty = $td.Substring(0, 4)
 $tm = $td.Substring(4, 2)
 
 Write-Host "---------------------------"
-
+Write-Host "Portfolio-huxo"
+Write-Host "--------------"
 view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td-huxo.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
 
 Write-Host "---------------------------"
-
+Write-Host "Portfolio-szst"
+Write-Host "--------------"
 view_csv E:\Data\Projects\Deploy-Operation\positions\$ty\$tm\持仓汇总-$td-szst.xls --chead 18 --where "合约.str.match('$match')" --sort "合约,策略账户" --pivot --values "买货值,卖货值,总货值" --indexes 策略账户 --aggfunc sum --precision 2
